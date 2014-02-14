f120688a413545daaa829f82dba661fc00cb262c
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-02-14 21:02:08 +0100

    templates in multiple files

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
- posts = new Meteor.Collection('posts');
- kurser = new Meteor.Collection('kurser');
-
-
  if (Meteor.isClient) {
-
-     Template.posts.count = function() {
-         return posts.find().count();
-     };
-
-     Template.kurser.count = function() {
-         return kurser.find().count();
-     };
 
      Template.test01.data01 = function() {
          return " this is data01 ";
@@ -28,38 +16,5 @@
 
  if (Meteor.isServer) {
 
-     if (posts.find().count() === 0) {
-         posts.insert({
-             title: 'new post 01 from server-side',
-             adresse: 'home'
-         });
-
-         posts.insert({
-             title: 'new post 02 from server-side',
-             adresse: 'home'
-         });
-         posts.insert({
-             title: 'new post 03 from server-side',
-             adresse: 'home'
-         });
-
-     };
-
-     if (kurser.find().count() === 0) {
-         kurser.insert({
-             title: 'new kursus 01 from server-side',
-             adresse: 'KBG'
-         });
-
-         kurser.insert({
-             title: 'new kursus 02 from server-side',
-             adresse: 'KBG'
-         });
-         kurser.insert({
-             title: 'new kursus 03 from server-side',
-             adresse: 'KBG'
-         });
-
-     };
 
  };
