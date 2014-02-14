f64e3b1c369bba8436c9fbf750ff561323a28239
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-02-14 21:51:39 +0100

    03_mongodb_data_show

--- test.js
+++ test.js
@@ -17,14 +17,14 @@
          return kurser.find().count();
      };
 
-     Template.test01.data01 = function() {
-         return " this is data01 ";
+     Template.test01.posts = function() {
+         return posts.find();
      };
-     Template.test02.data02 = function() {
-         return " this is data02 ";
+     Template.test02.kurser = function() {
+         return kurser.find();
      };
-     Template.test03.data03 = function() {
-         return " this is data03 ";
+     Template.test03.elever = function() {
+         return elever.find();
      };
 
 
