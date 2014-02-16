3d7e8bce6918de39f506c9ef20eaa8f13b232222
Jalal Hejazi <Jalal.Hejazi@gmail.com>
2014-02-16 14:46:16 +0100

    opgaver fra remote service

--- client/templates/opgaver/opgaver.js
+++ client/templates/opgaver/opgaver.js
@@ -1,27 +1,46 @@
-// her kan du oprette template til about siden
+  config = {
+      url: 'http://rest.supermobile.dk/opgaver/.json',
+      sessionKey: 'opgave',
+      getSession: function() {
+          return Session.get(this.sessionKey);
+      }
+  };
 
-Template.opgaverTemplate.title = function() {
+  Template.opgaverTemplate.title = function() {
+      return "opgaver fra remote service til mongodb til browser"
+  };
 
-    return "opgaver fra remote service"
+  Template.opgaverTemplate.opgave = function() {
 
-};
+      hentMineOpgaverFraService(config)
+
+      .done(function(response) {
+          Session.set(config.sessionKey, response.opgaver);
+          mongodb_opgaver_insert(response.opgaver)
+      });
+
+      return config.getSession();
+
+  };
+
+   // send ajax request og returnere promise object
+  var hentMineOpgaverFraService = function(config) {
+      return $.ajax(config);
+  };
 
 
-Template.opgaverTemplate.opgave = function() {
+   // denne function har et ansvar: at gemme alle opgaver i mongodb
+   // hvis ikke allerede har gjordt det før.
+   // så det ikke bliver for komplekst. vi kan altid reset mongodb
+   // Reset mongodb fra command-line> meteor reset 
+   // Test i browser console med> opgaver.find().fetch()
+  var mongodb_opgaver_insert = function(opgaver) {
 
-    var resource = 'opgave';
+      //for at undgå at fylde mongodb med redundant, sikres med count = 0
+      if (window.opgaver.find().count() === 0) {
+          // Der bliver oprettet kun én række i mongodb 
+          // rækken kan indeholde array of objects  [{},{},{}]  
+          window.opgaver.insert(opgaver);
+      };
 
-    jQuery.ajax({
-        url: 'http://rest.supermobile.dk/opgaver.json',
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(data, textStatus, xhr) {
-            Session.set(resource, data.opgaver);
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            Session.set(resource, errorThrown);
-        }
-    });
-
-    return Session.get(resource);
-};
+  };
